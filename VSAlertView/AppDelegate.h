894d5696ee446c3dedb61433dd7b171e645c7a7c
Vijay Subramaniyam <vijay.subramaniyam@nowfloats.com>
2015-05-06 20:33:49 +0530

    Initial Commit

---
+++ VSAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSAlertView
+//
+//  Created by Vijay subramaniyam on 06/05/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VSAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VSAlertView
+//
+//  Created by Vijay subramaniyam on 06/05/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
